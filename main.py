59113e341f4f9f4c6e3251030295c90777a32d62
Roberto Dijo <robertodijo@gmail.com>
2018-01-10 12:52:56 +0100

    first coding BTC to EUR and USD

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import requests
 import subprocess
 
 r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-subprocess.call('cd /', shell=True)
-subprocess.call('clear', shell=True)
+# subprocess.call('cd /', shell=True)
+# subprocess.call('clear', shell=True)
 
-print('The current price of Bitcoin is: ' + r.json()['bpi']['GBP']['rate'] + " pounds.")
+print('The current price of Bitcoin is: ' + r.json()['bpi']['EUR']['rate'] + " " + r.json()['bpi']['EUR']['code'])
+print('The current price of Bitcoin is: ' + r.json()['bpi']['USD']['rate'] + " " + r.json()['bpi']['USD']['code'])
 print('Last Updated: ' + r.json()['time']['updated'])
-print('Bitcoin value is displayed via the Coindesk API.')
+# print('Bitcoin value is displayed via the Coindesk API.')
+print(r.json()['disclaimer'])
+# print(r.json())
